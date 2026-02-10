b0bf6fb1a60b5e6e8e8dce3ad680ce8f75a1afc6
FKP27 <fkulcs@DESKTOP-5A8147U.>
2026-02-10 12:36:26 +0100

    Add MO occupancies, HOMO/LUMO labels, and styling

--- molcalc_lib/gamess_results.py
+++ molcalc_lib/gamess_results.py
@@ -1,7 +1,87 @@
+from typing import List, Tuple
+
 import numpy as np
 
+from ppqm import chembridge
 from ppqm import units
 from ppqm.utils import files as misc
+
+
+def _assign_orbital_occupancies(
+    energies: np.ndarray, n_electrons: int, tol_hartree: float = 1e-4
+) -> np.ndarray:
+    """
+    Assign orbital occupancies using Aufbau + Hund's rule for degenerate sets.
+    Returns an array of 0/1/2 occupancies with the same length as energies.
+    """
+    n_orb = len(energies)
+    occ = np.zeros(n_orb, dtype=int)
+    if n_orb == 0 or n_electrons <= 0:
+        return occ
+
+    order = np.argsort(energies)
+    remaining = int(n_electrons)
+    i = 0
+
+    while i < n_orb and remaining > 0:
+        group = [order[i]]
+        e0 = energies[order[i]]
+        i += 1
+
+        while i < n_orb and abs(energies[order[i]] - e0) <= tol_hartree:
+            group.append(order[i])
+            i += 1
+
+        # Fill singly first (Hund's rule)
+        for idx in group:
+            if remaining <= 0:
+                break
+            occ[idx] += 1
+            remaining -= 1
+
+        # Then pair
+        for idx in group:
+            if remaining <= 0:
+                break
+            if occ[idx] < 2:
+                occ[idx] += 1
+                remaining -= 1
+
+    return occ
+
+
+def _get_homo_lumo_indices(
+    energies: np.ndarray, occ: np.ndarray, tol_hartree: float = 1e-4
+) -> Tuple[List[int], List[int]]:
+    """Return lists of HOMO/LUMO indices, grouping degenerate levels."""
+    homo_indices: List[int] = []
+    lumo_indices: List[int] = []
+
+    if len(energies) == 0 or len(occ) == 0:
+        return homo_indices, lumo_indices
+
+    filled = np.where(occ > 0)[0]
+    if filled.size == 0:
+        return homo_indices, lumo_indices
+
+    homo_idx = int(filled.max())
+    homo_energy = energies[homo_idx]
+    homo_indices = [
+        i for i, e in enumerate(energies) if abs(e - homo_energy) <= tol_hartree
+    ]
+
+    # LUMO: first unoccupied above HOMO
+    for i in range(homo_idx + 1, len(energies)):
+        if occ[i] == 0:
+            lumo_energy = energies[i]
+            lumo_indices = [
+                j
+                for j, e in enumerate(energies)
+                if abs(e - lumo_energy) <= tol_hartree
+            ]
+            break
+
+    return homo_indices, lumo_indices
 
 
 def view_gamess_calculation(calculation):
@@ -43,6 +123,19 @@ def view_gamess_calculation(calculation):
     else:
         data["name"] = calculation.name
 
+    # Total electron count from stored SDF (sum atomic numbers - formal charge)
+    data["electron_count"] = None
+    try:
+        sdfstr = data.get("sdf")
+        if sdfstr:
+            molobj = chembridge.sdfstr_to_molobj(sdfstr)
+            if molobj is not None:
+                atoms = chembridge.get_atoms(molobj, type=int)
+                charge = chembridge.get_charge(molobj)
+                data["electron_count"] = int(np.sum(atoms) - charge)
+    except Exception:
+        data["electron_count"] = None
+
     fmt = "{:.2f}"
 
     data["enthalpy"] = fmt.format(data["enthalpy"] * units.calories_to_joule)
@@ -77,10 +170,19 @@ def view_gamess_calculation(calculation):
     data["s_vibra"] = fmt.format(thermotable[3, 5])
     data["s_total"] = fmt.format(thermotable[4, 5])
 
-    # Molecular orbitals format
-    data["orbitals"] = misc.load_array(data["orbitals"])
-    data["orbitals"] *= units.hartree_to_ev
-    data["orbitals"] = [fmt.format(x) for x in data["orbitals"]]
+    # Molecular orbitals format + occupancy
+    orbitals = misc.load_array(data["orbitals"])
+    if data["electron_count"] is None:
+        data["orbital_occupancies"] = []
+        data["homo_indices"] = []
+        data["lumo_indices"] = []
+    else:
+        occ = _assign_orbital_occupancies(orbitals, data["electron_count"])
+        data["orbital_occupancies"] = occ.tolist()
+        homo_indices, lumo_indices = _get_homo_lumo_indices(orbitals, occ)
+        data["homo_indices"] = homo_indices
+        data["lumo_indices"] = lumo_indices
+    data["orbitals"] = [fmt.format(x) for x in orbitals * units.hartree_to_ev]
 
     # Vibrational Frequencies format
     data["vibfreq"] = misc.load_array(data["vibfreq"])
